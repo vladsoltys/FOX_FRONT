9b72953fad069ea519599e0ed51dfcbed819433f
vladsoltys <soltisvlad97@gmail.com>
2026-01-03 22:58:09 +0200

    20_tasks_done

--- before_mentoring_tasks_p1/task_2.js
+++ before_mentoring_tasks_p1/task_2.js
@@ -39,4 +39,27 @@ const b = !A && !B
 const c = !(A && C) || B
 const d = A && !(B && C)
 
-console.log(a, b, c, d)
+// console.log(a, b, c, d)
+
+/* Record the condition that is true when:
+
+     a) each of the A and B numbers is more than 100;
+
+     b) at least one of A and B numbers is positive;
+
+     c) each of the A, B,C numbers is multiple of three;
+
+     d) at least one of the A, B, C numbers is negative. */
+
+const num_A = 600;
+const num_B = 300;
+const num_C = -30;
+
+const cond_a = num_A > 100 && num_B > 100
+const cond_b = num_A > 0 || num_B > 0
+const cond_c = num_A % 3 === 0 && num_B % 3 === 0 && num_C % 3 === 0
+const cond_d = num_A < 0 || num_B < 0 || num_C < 0
+
+
+console.log(cond_a, cond_b, cond_c, cond_d)
+
