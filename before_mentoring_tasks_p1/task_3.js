f841e0134c078e2c11e811b89dc1aabdbc1f2230
vladsoltys <soltisvlad97@gmail.com>
2026-01-04 20:05:10 +0200

    22_tasks_done

---
+++ before_mentoring_tasks_p1/task_3.js
@@ -0,0 +1,11 @@
+/* Type in a "column": all the integers from 20 to 35. */
+
+for (let i = 20 ; i <= 35; i++) {
+    // console.log(i)
+}
+
+/* Type in a "column": all the integers from 90 to 55. */
+
+for (let i = 90 ; i >= 55; i--) {
+    console.log(i)
+}
