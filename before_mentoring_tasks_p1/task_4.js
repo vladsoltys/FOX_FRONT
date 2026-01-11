93fb7d2f961325359aeb38cc09dd720b57ca64e8
vladsoltys <soltisvlad97@gmail.com>
2026-01-11 21:04:42 +0200

    ordered and improved

--- before_mentoring_tasks_p1/task_4.js
+++ before_mentoring_tasks_p1/task_4.js
@@ -2,99 +2,77 @@
 Fill the elements with random numbers from 1 to 10. 
 Display the elements of the array in one row in the console. */
 
-let arr = [];
+function arrCreate (min, max, len) {
+    let arr = [];
 
-for (let i = 1; i <= 20; i++) {
-    arr.push(Math.floor((Math.random() * 10) + 1))
+    for (let i = 1; i <= len; i++) {
+        arr.push(Math.floor(Math.random() * (max - min + 1) + min))
+    }
+
+    return arr
 }
 
-// console.log(arr.join(', '));
+// console.log(arrCreate(1, 10, 20).join(', '));
 
 /* Create a univariate array of int type, with the length of 15. 
 Fill the elements with random numbers from 10 to 20. 
 Count and display the sum of array elements in the console. */
 
-// function arrCreate () {
-//     let arr = [];
+let arr = arrCreate(10, 20, 15);
 
-//     for (let i = 1; i <= 15; i++) {
-//         arr.push(Math.floor((Math.random() * 11) + 10))
-//     }
+function sum(arr) {
+    let sum = 0;
+    
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i]
+    }
 
-//     return arr
-// }
+    return sum
+}
 
-// const arr_2 = arrCreate();
-
-// let sum = 0;
-
-// for (let i = 0; i < arr_2.length; i++) {
-//     sum += arr_2[i]
-// }
-
-// console.log(arr_2)
-// console.log(sum);
+// console.log(arr)
+// console.log(sum(arr));
 
 /* Create a univariate array of int type, with the length of 20. 
 Fill the elements with random numbers from -50 to 50. 
 Count and display the sum of all the positive array elements in the console. */
 
-// function arrCreate () {
-//     let arr = [];
-//     const min = -50;
-//     const max = 50;
+arr = arrCreate(-50, 50, 20)
 
-//     for (let i = 1; i <= 20; i++) {
-//         arr.push(Math.floor(Math.random() * (max - min + 1) + min))
-//     }
+function sumPositive (arr) {
+    let sum = 0;
+    
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i] > 0) {
+            sum += arr[i]
+        }
+    }
 
-//     return arr
-// }
+    return sum
+}
 
-// const arr_2 = arrCreate()
-
-// let sum = 0;
-
-// for (let i = 0; i < arr_2.length; i++) {
-//     if(arr_2[i] > 0) {
-//         sum += arr_2[i]
-//     }
-// }
-
-// console.log(arr_2)
-// console.log(sum);
+// console.log(arr)
+// console.log(sumPositive(arr));
 
 /* Create a univariate array of int type, with the length of 20. 
 Fill the elements with random numbers from 0 to 100. 
 Find the element (index) that has the highest value in the array and display it in the console. */
 
-// function arrCreate () {
-//     let arr = [];
-//     const min = 0;
-//     const max = 100;
+arr = arrCreate(0, 100, 20);
 
-//     for (let i = 1; i <= 20; i++) {
-//         arr.push(Math.floor(Math.random() * (max - min + 1) + min))
-//     }
+function maxInArray(arr){
+  let maxIndx = 0;
 
-//     return arr
-// }
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > arr[maxIndx]) {
+      maxIndx = i;
+    }
+  }
+  return `${maxIndx} - ${arr[maxIndx]}`
+};
 
-// const arr_2 = arrCreate()
-
-// function maxInArray(arr){
-//   let maxIndx = 0;
-
-//   for (let i = 1; i < arr.length; i++) {
-//     if (arr[i] > arr[maxIndx]) {
-//       maxIndx = i;
-//     }
-//   }
-//   return `${maxIndx} - ${arr[maxIndx]}`
-// };
-
-// console.log(arr_2)
-// console.log(maxInArray(arr_2));
+// console.log(arr)
+// console.log(maxInArray(arr));
 
 /* The height of 20 people is known. Find out the mean value of a height:
 
@@ -102,19 +80,7 @@ Create a univariate array of int type, with the length of 20.
 Fill the elements with random numbers from 150 to 195. 
 Count and display the arithmetic mean of array elements in the console (mean value of a height). */
 
-function arrCreate () {
-    let arr = [];
-    const min = 150;
-    const max = 195;
-
-    for (let i = 1; i <= 20; i++) {
-        arr.push(Math.floor(Math.random() * (max - min + 1) + min))
-    }
-
-    return arr
-}
-
-const arr_2 = arrCreate()
+arr = arrCreate(150, 195, 20);
 
 function arrMean(arr){
     let sum = 0;
@@ -127,5 +93,5 @@ function arrMean(arr){
 };
 
 
-console.log(arr_2)
-console.log(arrMean(arr_2));
+// console.log(arr)
+// console.log(arrMean(arr));
