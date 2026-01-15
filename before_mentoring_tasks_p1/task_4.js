7542ff39ba28a112d8d8e6058edb57881e207925
Vlad Soltys <soltisvlad97@gmail.com>
2026-01-15 16:31:53 +0200

    37 done

--- before_mentoring_tasks_p1/task_4.js
+++ before_mentoring_tasks_p1/task_4.js
@@ -98,26 +98,69 @@ function arrMean(arr){
 
 /* Ask a user to type a raw of English text, count how many times there are a, u, i letters. */
 
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+// const readline = require('readline').createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
 
-readline.question('Type a raw of English text: ', (txt) => {
-    function letterCount(txt) {
-        let count = 0;
+// readline.question('Type a raw of English text: ', (txt) => {
+//     function letterCount(txt) {
+//         let count = 0;
 
-        for(let i = 0; i < txt.length; i++) {
-            if(/[aui]/.test(txt[i])) {
-                count ++
-            }
+//         for(let i = 0; i < txt.length; i++) {
+//             if(/[aui]/.test(txt[i])) {
+//                 count ++
+//             }
+//         }
+//         return count;
+//     }
+
+//     console.log(letterCount(txt))
+//     readline.close();
+// });
+
+/* An array of integers is given. Write a code that will turn every negative element of the array into positive ones.
+
+
+Display the result in the console.
+
+For example, there are:
+
+[10, -20, 30, -40, 50]
+
+
+The result: 
+
+[10, 20, 30, 40, 50]
+
+
+PS. Data should be saved in the same array */
+
+arr = [10, -20, 30, -40, 50]
+
+function inPositiveConvert(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] < 0) {
+            arr[i] *= -1
         }
-        return count;
     }
 
-    console.log(letterCount(txt))
-    readline.close();
-});
+    return arr
+}
+
+inPositiveConvert(arr)
+console.log(arr)
+
+/* Create a univariate array of int type, with the length of N. 
+Fill the elements with random numbers from 1 to 100. 
+Swap the minimum and maximum array elements.
+For example there are:
+[20, 10, 30, 50, 40, 45]
+
+The result: 
+
+[20, 50, 30, 10, 40, 45] */
+
 
 
 
